e8e221b2bfc302bf68e8cd74320f4c89c3142055
Pommerening <Thomas.Pommerening@haw-hamburg.de>
2017-09-26 18:18:25 +0200

    minor stuff

--- JS/Intro.js
+++ JS/Intro.js
@@ -3,3 +3,15 @@ var a = 1;
 var b = 2;
 console.log(a);
 console.log(b);
+//operatoren
+ var foo = 1;
+ var bar = 2;
+ 
+ var result = (foo == bar);
+ console.log(result);
+ 
+ //
+// if else wie java sowie while und for
+function meineFunktion(){
+		console.log("function activated")
+}
